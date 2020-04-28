9ebf9ee3c9a1e7b2c40cb84a2279ea45ff198f79
Allen <allenwang52@yahoo.com>
2020-04-27 23:54:30 -0500

    Added buttons and logo to app

--- src/Fostr_ReactNative/App.js
+++ src/Fostr_ReactNative/App.js
@@ -1,21 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Image, Alert, SafeAreaView, Dimensions } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Fostr</Text>
-      <Text>The to-go app for fostering pets!</Text>
-      <Button title="Get Started!" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Image
+          source={require('./assets/FostrLogo.png')}
+        />
+        <Text>The to-go app for fostering pets!</Text>
+      </View>
+
+      <View style={{flexDirection: "row", alignItems: "center", padding: 25 }}>
+        <TextInput
+          placeholder="Name"
+          style={{borderBottomColor:"black", borderBottomWidth: 1, padding: 3}}
+        />
+        <Button title="Submit" />
+      </View>
+
+      <View style={styles.bottom}>
+        <Button title="No" color="gray" onPress={() => Alert.alert("Disliked!")} />
+        <Button title="info" color="lightblue" onPress={() => Alert.alert("Here's the info on this animal!")} />
+        <Button title="Yes" color="red" onPress={() => Alert.alert('Liked!')} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexGrow: 1,
+    backgroundColor: "purple",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bottom: {
+    width: Dimensions.get('window').width,
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "",
   },
 });
