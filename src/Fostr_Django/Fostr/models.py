1160586a3f58fcbf0b62ed038633980d00842d75
Allen <allenwang52@yahoo.com>
2020-04-28 16:02:26 -0500

    Added adopter, adoptee, and pet models

--- src/Fostr_Django/Fostr/models.py
+++ src/Fostr_Django/Fostr/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class PhoneModel(models.Model):
+    phone_regix = RegexValidator(regex=r"^\+?1?\d{9,15}$", message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regix], max_length=17, blank=True)
+
+class Adopter(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.PositiveSmallIntegerField(default=0)
+    phone_number = PhoneModel()
+
+class Adoptee(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.PositiveSmallIntegerField(default=0)
+    phone_number = PhoneModel()
+
+class Pet(models.Model):
+    adoptee = models.ForeignKey(Adoptee, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=25)
