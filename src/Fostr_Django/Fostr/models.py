b923ed90fc78f108226f3929050ab9d4e790cc0c
arvinbhatti <arvinbhatti@gmail.com>
2020-05-14 20:54:06 -0500

    added some constructors

--- src/Fostr_Django/Fostr/models.py
+++ src/Fostr_Django/Fostr/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.contrib.gis.geoip2 import GeoIP2
+
+import socket
 
 SPECIES_CHOICES = (
     (CAT, "Cat"),
@@ -11,18 +14,53 @@ class PhoneModel(models.Model):
     phone_number = models.CharField(validators=[phone_regix], max_length=17, blank=True)
 
 class Adopter(models.Model):
+
+    def __init__(self, name, age, phone_number):
+        self.name = name
+        self.age = age
+        self.coords = GeoIP2.coords(socket.gethostbyname(socket.gethostname()))
+        self.phone_number = phone_number
+        #make json and put on AWS?
+
+
     name = models.CharField(max_length=50)
     age = models.PositiveSmallIntegerField(default=0)
+    coords = []
     phone_number = PhoneModel()
 
-class Adoptee(models.Model):
+class Foster(models.Model):
+
+    def __init__(self, name, age, phone_number):
+        self.name = name
+        self.age = age
+        self.coords = GeoIP2.coords(socket.gethostbyname(socket.gethostname()))
+        self.phone_number = phone_number
+
+    def add_pet(self, pet):
+        self.pets.append(pet)
+
+    def remove_pet(self, pet):
+        self.pets.remove(pet)
+
     name = models.CharField(max_length=50)
     age = models.PositiveSmallIntegerField(default=0)
+    coords = []
     phone_number = PhoneModel()
+    pets = []  #should be unique IDs
 
 class Pet(models.Model):
-    adoptee = models.ForeignKey(Adoptee, on_delete=models.SET_NULL)
+
+    def __init__(self, foster, name, age, species, breed):
+        self.foster = foster
+        self.name = name
+        self.age = age
+        self.species = species
+        self.breed = breed
+
+    foster = models.ForeignKey(Foster, on_delete=models.SET_NULL) #should be unique ID
     name = models.CharField(max_length=25)
+    age = models.PositiveSmallIntegerField(default=0)
     species = models.CharField(max_length=9,
                                 choice=SPECIES_CHOICES
                                 default=DOG)
+    breed = models.CharField(max_length=25)
