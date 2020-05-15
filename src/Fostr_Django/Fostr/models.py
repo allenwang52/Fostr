d70f5dfd55fa50e56d8ca08d88b63c732f310530
arvinbhatti <arvinbhatti@gmail.com>
2020-05-14 19:34:05 -0500

    added 2 lines

--- src/Fostr_Django/Fostr/models.py
+++ src/Fostr_Django/Fostr/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.core.validators import RegexValidator
 
+SPECIES_CHOICES = (
+    (CAT, "Cat"),
+    (DOG, "Dog"),
+)
 
 class PhoneModel(models.Model):
     phone_regix = RegexValidator(regex=r"^\+?1?\d{9,15}$", message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
@@ -19,3 +23,6 @@ class Adoptee(models.Model):
 class Pet(models.Model):
     adoptee = models.ForeignKey(Adoptee, on_delete=models.SET_NULL)
     name = models.CharField(max_length=25)
+    species = models.CharField(max_length=9,
+                                choice=SPECIES_CHOICES
+                                defailt=DOG)
